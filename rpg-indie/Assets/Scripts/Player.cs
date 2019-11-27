04d04b8107de5b4be2cc6bda281a74151392e2cd
patriciamesttre <patriciamesttre@hotmail.com>
2019-11-27 20:34:15 -0300

    Atualização

---
+++ rpg-indie/Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float velocidade = 0.01f;
+
+    private Animator controleAnimacao;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.controleAnimacao = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direcao = new Vector3();
+        direcao.y = Input.GetAxis("Vertical");
+        direcao.x = Input.GetAxis("Horizontal");
+
+        this.transform.position += direcao * velocidade;
+
+        bool estouAndando = direcao.magnitude != 0;
+        this.controleAnimacao.SetBool("estaAndando", estouAndando);
+        this.controleAnimacao.SetFloat("velocidadeY", direcao.y);
+        this.controleAnimacao.SetFloat("velocidadeX", direcao.x);
+
+
+    }
+}
